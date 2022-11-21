a129c8cc7a4c0dc89638d92059d69694694b7d53
Vladimir Abubakirov <abubakirov04@mail.ru>
2022-11-22 00:48:41 +0300

    Added first iteration

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "stdio.h"
+#include "string.h"
+#include "assert.h"
+
+char* string = nullptr;
+
+int getN() {
+    int val = 0;
+    const char* oldS = string;
+
+    if ('0' <= *string && '9' >= *string) {
+        val = *string - '0';
+        string++;
+    }
+
+    assert(string != oldS);
+
+    return val;
+}
+
+int getG() {
+    int val = getN();
+
+    assert(*string == '\0');
+
+    return val;
+}
+
+int main() {
+    char buffer[4096] = "";
+    scanf("%s", buffer);
+    string = strdup(buffer);
+    printf("%d", getG());
+
+    return 0;
+}
